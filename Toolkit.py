495992bb317f0481d76b9fe5f117539790e7cadb
Kevin <sturgiskev@gmail.com>
2012-12-12 23:16:54 -0600

    modified toolkit

--- Toolkit.py
+++ Toolkit.py
@@ -2,7 +2,10 @@
 import os
 import sys
 import library.menu
+import library.console
 
-if sys.argv ==2:
-	if sys.argv[1] = console:
-		library.menu()
+if len(sys.argv) ==2:
+	if sys.argv[1] = "menu":
+		library.menu.Menu()
+	elif sys.argv[1] = "console":
+		library.console.console()
